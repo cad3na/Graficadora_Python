afdb8cd9ba439e7f384feb6db11279e9088c7aa9
robblack007 <robblack00_7@hotmail.com>
2013-04-02 13:57:08 -0600

    Se describe el proyecto en README.md
    Se describe el proyecto en el archivo README y se agregan cabeceras a
    los archivos en donde se atribuye el codigo al autor.

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ Graficadora_Python
 
 Esta es una implementacion de Ttk para graficar puntos en Python.
 
-La principal motivacion detras de esta implementacion es la de comprender la forma en que se usa Ttk para crear GUI's en Python
+La principal motivación detras de esta implementación es la de comprender la forma en que se usa Ttk para crear GUI's en Python.
+
+Eres bienvenido a forkear este repositorio si necesitas ejemplos de como usar Ttk, si estas curioso en como podrias graficar funciones, o simplemente quieres graficar alguna funcion sin tener que adquirir software caro.
+
+Por el momento no deseo recibir colaboracion en este proyecto, sin embargo si tienes alguna sugerencia de como podria hacer mejor alguna funcion, o de funciones que te gustaria ver puedes dejar tus comentarios.
--- prueba.py
+++ prueba.py
@@ -1,3 +1,18 @@
+#################################################################################
+#Proyecto: Graficadora Python
+#Archivo: prueba.py
+#Descripcion: En este archivo se encuentran definidas todas las clases necesarias
+#             para el funcionamiento basico de las matematicas asociadas al 
+#             dibujo de formas basicas y la extraccion de parametros asociados a
+#             estas.
+#Autor: Cadena Vega Roberto
+#Version: 0.2
+#Notas de la Version: Clases Vector y Linea terminadas, clase Poligono 
+#                     aun no concluida; para los metodos extraer_vertices,
+#                     calcular_area y revisar_lados aun no se tienen maneras
+#                     eficientes de implementar.
+#Fecha: 14/02/13
+#################################################################################
 from graficar import Graficadora
 from math import sin, pi
 
